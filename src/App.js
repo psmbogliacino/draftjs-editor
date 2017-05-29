ab56d454960e965f7658978b63b0c14f1e5464e2
Bogliacino Mauricio <mbogliacino@primerosystems.com>
2017-05-29 16:35:23 -0300

    Link Demo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import MyEditor from './MyEditor'
+import logo from './logo.svg';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <div className="App-intro">
+          Article:
+
+          <div className="editor-wrapper">
+          <MyEditor />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './css/index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
